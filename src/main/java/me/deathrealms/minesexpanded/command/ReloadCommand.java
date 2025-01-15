adc34320c83b335f5d799d7ef8308ddbcc3b1cfa
DeathRealms <justintoolove@gmail.com>
2025-01-15 00:22:58 -0500

    Reload command now reloads all mines

--- src/main/java/me/deathrealms/minesexpanded/command/ReloadCommand.java
+++ src/main/java/me/deathrealms/minesexpanded/command/ReloadCommand.java
@@ -1,11 +1,16 @@
 package me.deathrealms.minesexpanded.command;
 
+import me.deathrealms.minesexpanded.Mine;
+import me.deathrealms.minesexpanded.MineRegistry;
+import me.deathrealms.minesexpanded.MineServices;
 import me.deathrealms.minesexpanded.MinesExpanded;
 import me.deathrealms.minesexpanded.util.Message;
 import me.deathrealms.minesexpanded.util.MessageUtil;
 import org.bukkit.command.CommandSender;
 import org.incendo.cloud.bukkit.BukkitCommandManager;
 import org.incendo.cloud.description.Description;
+
+import java.util.List;
 
 public class ReloadCommand implements MECommand {
 
@@ -18,6 +23,19 @@ public class ReloadCommand implements MECommand {
                         .handler(context -> {
                             plugin.reloadConfig();
                             MessageUtil.loadMessages(plugin);
+
+                            MineRegistry registry = plugin.mineRegistry();
+                            MineServices services = plugin.mineServices();
+
+                            List<Mine> mines = registry.getMines().values().stream().toList();
+
+                            for (Mine mine : mines) {
+                                services.removeService(mine);
+                            }
+
+                            registry.registerMines();
+                            services.registerServices();
+
                             MessageUtil.message(context.sender(), Message.RELOAD_SUCCESS);
                         })
         );
